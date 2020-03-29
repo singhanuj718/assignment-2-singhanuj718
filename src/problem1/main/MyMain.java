70a150f723ea458983c0caae367dec47d5c52369
Anuj Kumar Singh <54413107+singhanuj718@users.noreply.github.com>
2020-03-29 13:14:21 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,17 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+ public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(85);
+        bst.insert(25);
+        bst.insert(45);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(15);
+        bst.insert(10);
+        bst.insert(1);
+        bst.diaplay(bst.getRoot(), bst.getMlevel());
+        bst.d2();
+    }
 }
